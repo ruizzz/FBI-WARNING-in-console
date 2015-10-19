9b5bcd16c9df170ed4965802df432619ac1f8415
Rui Zhao <zhao.rui@me.com>
2015-10-18 23:54:00 -0400

    Added background color

--- fbi_warning.sh
+++ fbi_warning.sh
@@ -1,8 +1,10 @@
-echo "$(tput cuf 10) $(tput setab 1)FBI WARNING$(tput sgr 0)"
-echo ""
-echo "Federal Law provides severe civil and criminal penalties for
-the unauthorized reproduction, distribution, or exhibition of
-copyrighted motion pictures (Title 17, United States Code,
-Sections 501 and 508). The Federal Bureau of Investigation
-investigates allegations of criminal copyright infringement"
-echo "$(tput cuf 5) (Title 17, United States Code, Section 506)."
+echo -e "$(tput setab 0)\t\t\t\t\t\t\t       $(tput sgr 0)"
+echo -e "$(tput setab 0)\t\t\t   $(tput setab 9)FBI WARNING$(tput setab 0)\t\t\t       $(tput sgr 0)"
+echo -e "$(tput setab 0)\t\t\t\t\t\t\t       $(tput sgr 0)"
+echo "$(tput setab 0) Federal Law provides severe civil and criminal penalties for  $(tput sgr 0)
+$(tput setab 0) the unauthorized reproduction, distribution, or exhibition of $(tput sgr 0)
+$(tput setab 0) copyrighted motion pictures (Title 17, United States Code,    $(tput sgr 0)
+$(tput setab 0) Sections 501 and 508). The Federal Bureau of Investigation    $(tput sgr 0)
+$(tput setab 0) investigates allegations of criminal copyright infringement   $(tput sgr 0)"
+echo -e "$(tput setab 0)\t(Title 17, United States Code, Section 506).\t       $(tput sgr 0)"
+echo -e "$(tput setab 0)\t\t\t\t\t\t\t       $(tput sgr 0)"
